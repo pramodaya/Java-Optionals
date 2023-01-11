7dc18462b34655294df4d3c682b3332e5b9a22e4
pramodaya <pramodayajayalath@gmail.com>
2023-01-11 22:38:17 +0200

    Optional code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /**
+         * Imperative older way of checking 'NULL' values
+         */
+        String firstName = "Pramodaya";
+        if(firstName != null){
+            firstName = firstName.toUpperCase();
+        }else{
+            //do something
+        }
+
+
+        /**
+         * Declarative modern way of handling 'NULL' values
+         */
+        //Optional variable
+        Optional<Object> empty = Optional.empty();
+
+        //Not null value - cannot be null for sure
+        Optional<String> helloWorld = Optional.of("Hello world");
+
+       //Value can be null or not
+        Optional<Object> nullOptional = Optional.ofNullable(null);
+
+        System.out.println(helloWorld
+                .map(String::toUpperCase)
+                .orElse("Cannot make it Uppercase"));
+
+        /**
+         * Methods in Optionals
+         */
+        System.out.println(empty.isPresent());
+        System.out.println(empty.isEmpty());
+        helloWorld.orElse("Something");
+
+        helloWorld
+                .map(String::toUpperCase)
+                .orElse("Cannot make it Uppercase");
+
     }
 }
